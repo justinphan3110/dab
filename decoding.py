81db94ee4fc0fc90afd8a3e225cf084757a1e03d
Long Phan <44376091+justinphan3110@users.noreply.github.com>
2021-06-04 15:41:53 +0700

    batch size for t2t decoding

--- decoding.py
+++ decoding.py
@@ -358,7 +358,7 @@ def evaluate_from_file_fn(estimator,
         #   lambda ex: ({"inputs": tf.reshape(ex["inputs"], (length, 1, 1)), "targets": tf.reshape(ex["targets"], (length, 1, 1))}, tf.reshape(ex["targets"], (length, 1, 1))) )
 
         dataset = dataset.map(
-          lambda ex: ({"inputs": tf.reshape(ex["inputs"], (128, 1, 1)), "targets": tf.reshape(ex["targets"], (128, 1, 1))}, tf.reshape(ex["targets"], (length, 1, 1))) )
+          lambda ex: ({"inputs": tf.reshape(ex["inputs"], (length, 1, 1)), "targets": tf.reshape(ex["targets"], (length, 1, 1))}, tf.reshape(ex["targets"], (length, 1, 1))) )
 
         return dataset
   loss = estimator.evaluate(eval_input_fn, steps=1, checkpoint_path=checkpoint_path)
