f68d375c8885e12a9694ad0c49702a7594adbf93
Long Phan <44376091+justinphan3110@users.noreply.github.com>
2021-06-07 15:58:48 +0700

    add dummy estimator

--- decoding.py
+++ decoding.py
@@ -111,7 +111,7 @@ def get_model_fn(model_name, hparams, init_checkpoint):
     # Accumulate losses
     loss = sum(losses_dict[key] for key in sorted(losses_dict.keys()))
     
-    scaffold_fn = this_model.get_scaffold_fn(init_checkpoint)
+    scaffold_fn = None
 
 
     print('logits ', logits)
