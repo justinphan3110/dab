3f04feb38b358a07a16d55255c02b99d9c5dd21b
Long Phan <44376091+justinphan3110@users.noreply.github.com>
2021-06-29 19:42:14 +0700

    register transformer_tall9_extra_tokens

--- t2t_trainer.py
+++ t2t_trainer.py
@@ -10,6 +10,11 @@ from tensor2tensor.utils import registry
 import problems
 import tensorflow as tf
 
+flags = tf.flags
+FLAGS = flags.FLAGS
+flags.DEFINE_string('extra_tokens', 32 , 'extra tokens to be added into encoding vectors')
+
+
 
 @registry.register_hparams
 def transformer_tall9():
@@ -20,6 +25,17 @@ def transformer_tall9():
   hparams.num_heads = 12
   return hparams
   
+
+@registry.register_hparams
+def transformer_tall9_extra_tokens():
+  hparams = transformer.transformer_big()
+  hparams.hidden_size = 768
+  hparams.filter_size = 3072
+  hparams.num_hidden_layers = 9
+  hparams.num_heads = 12
+  hparams.extra_tokens = FLAGS.extra_tokens
+  return hparams
+
 
 if __name__ == '__main__':
   tf.logging.set_verbosity(tf.logging.INFO)
--- train_tpus.py
+++ train_tpus.py
@@ -8,24 +8,23 @@ import subprocess
 # os.system("pip install google.colab")
 
 TPU_ADDRESSES = [
-
+    
 
 ]
+
+task = 'envi'
 for subset in range(0,1):
     subset=0
     total_train_steps = 500000
     use_tpu = True
     TPU_ADDRESS = TPU_ADDRESSES[subset]
-    # TPU wants the address to begin with gs://
-    train_output_dir = f'gs://best_vi_translation/checkpoints/pseudo_label_multicc_translate_envi_iwslt32k/subset/{subset}/'
-    train_data_dir = f'gs://best_vi_translation/data/pseudo_label_multicc_translate_envi_iwslt32k/subset/{subset}/'
-    # print(train_output_dir)
-    # print(train_data_dir)
+    train_output_dir = f'gs://best_vi_translation/checkpoints/pseudo_label_multicc_translate_{task}_iwslt32k/subset/{subset}/'
+    train_data_dir = f'gs://best_vi_translation/data/pseudo_label_multicc_translate_{task}_iwslt32k/subset/{subset}/'
     hparams_str = ('learning_rate_cosine_cycle_steps={},'
                 'max_length=128,batch_size=4096,'  # real batch_size = 4096/128
-                'learning_rate_constant=2.0').format(total_train_steps)
+                'learning_rate_constant=2.0').format(2000000)
     print(hparams_str)
     hparams_set = 'transformer_tall9'
-    problem = 'translate_envi_iwslt32k'
+    problem = 'translate_class11_pure_envi_iwslt32k'
     sleep(5)
     subprocess.Popen(shlex.split(f"python t2t_trainer.py --model=transformer --hparams_set={hparams_set} --hparams={hparams_str} --train_steps={total_train_steps} --eval_steps=20 --problem={problem} --data_dir={train_data_dir} --output_dir={train_output_dir} --cloud_tpu_name={TPU_ADDRESS} --use_tpu={use_tpu}"))
