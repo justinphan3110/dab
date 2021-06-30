0c80819db3e3a41025536f6141510618c82929af
Long Phan <44376091+justinphan3110@users.noreply.github.com>
2021-06-30 18:08:25 +0700

    update tpu address

--- train_tpus.py
+++ train_tpus.py
@@ -26,7 +26,7 @@ TPU_ADDRESSES = [
 ]
 
 task = 'envi'
-for subset in range(0,1):
+for subset in range(0,5):
     total_train_steps = 500000
     use_tpu = True
     TPU_ADDRESS = TPU_ADDRESSES[subset]
@@ -41,18 +41,18 @@ for subset in range(0,1):
     sleep(5)
     subprocess.Popen(shlex.split(f"python3 t2t_trainer.py --model=transformer --hparams_set={hparams_set} --hparams={hparams_str} --train_steps={total_train_steps} --eval_steps=20 --problem={problem} --data_dir={train_data_dir} --output_dir={train_output_dir} --use_tpu={use_tpu} --cloud_tpu_name=grpc://{TPU_ADDRESS}:8470 > nohup_{task}_{subset}.txt"))
 
-# task = 'vien'
-# for subset in range(0,5):
-#     total_train_steps = 500000
-#     use_tpu = True
-#     TPU_ADDRESS = TPU_ADDRESSES[4+subset]
-#     train_output_dir = f'gs://best_vi_translation/checkpoints/pseudo_label_multicc_translate_{task}_iwslt32k/subset/{subset}/'
-#     train_data_dir = f'gs://best_vi_translation/data/pseudo_label_multicc_translate_{task}_iwslt32k/subset/{subset}/'
-#     hparams_str = ('learning_rate_cosine_cycle_steps={},'
-#                 'max_length=128,batch_size=4096,'  # real batch_size = 4096/128
-#                 'learning_rate_constant=2.0').format(2000000)
-#     print(hparams_str)
-#     hparams_set = 'transformer_tall9'
-#     problem = f'pseudo_label_multicc_translate_{task}_iwslt32k'
-#     sleep(5)
-#     subprocess.Popen(shlex.split(f"python3 t2t_trainer.py --model=transformer --hparams_set={hparams_set} --hparams={hparams_str} --train_steps={total_train_steps} --eval_steps=20 --problem={problem} --data_dir={train_data_dir} --output_dir={train_output_dir} --use_tpu={use_tpu} --cloud_tpu_name=grpc://{TPU_ADDRESS}:8470 > nohup_{task}_{subset}.txt"))
+task = 'vien'
+for subset in range(0,5):
+    total_train_steps = 500000
+    use_tpu = True
+    TPU_ADDRESS = TPU_ADDRESSES[4+subset]
+    train_output_dir = f'gs://best_vi_translation/checkpoints/pseudo_label_multicc_translate_{task}_iwslt32k/subset/{subset}/'
+    train_data_dir = f'gs://best_vi_translation/data/pseudo_label_multicc_translate_{task}_iwslt32k/subset/{subset}/'
+    hparams_str = ('learning_rate_cosine_cycle_steps={},'
+                'max_length=128,batch_size=4096,'  # real batch_size = 4096/128
+                'learning_rate_constant=2.0').format(2000000)
+    print(hparams_str)
+    hparams_set = 'transformer_tall9'
+    problem = f'pseudo_label_multicc_translate_{task}_iwslt32k'
+    sleep(5)
+    subprocess.Popen(shlex.split(f"python3 t2t_trainer.py --model=transformer --hparams_set={hparams_set} --hparams={hparams_str} --train_steps={total_train_steps} --eval_steps=20 --problem={problem} --data_dir={train_data_dir} --output_dir={train_output_dir} --use_tpu={use_tpu} --cloud_tpu_name=grpc://{TPU_ADDRESS}:8470 > nohup_{task}_{subset}.txt"))
