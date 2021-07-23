cb5e80c0fc390d3848fa45ea3f5fab290d5095be
Long Phan <44376091+justinphan3110@users.noreply.github.com>
2021-07-23 14:19:10 +0700

    add train_tpus_custom_layer.py

--- train_tpus_custom_layer.py
+++ train_tpus_custom_layer.py
@@ -29,22 +29,22 @@ TPU_ADDRESSES = [
     '10.65.207.202'
 ]
 
-task = 'envi'
+task = 'vien'
 
 l = []
 for index, (encoder_layers,decoder_layers) in enumerate([[12,6], [15,3]]):
     total_train_steps = 500000
     use_tpu = True
     TPU_ADDRESS = TPU_ADDRESSES[index]
-    train_output_dir = f'gs://best_vi_translation/checkpoints/custom_layer_translate_class11_pure_envi_iwslt32k/{encoder_layers}_{decoder_layers}/'
-    train_data_dir = f'gs://best_vi_translation/data/translate_class11_pure_envi_iwslt32k/'
+    train_output_dir = f'gs://best_vi_translation/checkpoints/custom_layer_translate_class11_pure_{task}_iwslt32k/{encoder_layers}_{decoder_layers}/'
+    train_data_dir = f'gs://best_vi_translation/data/translate_class11_pure_{task}_iwslt32k/'
 
     hparams_str = ('learning_rate_cosine_cycle_steps={},'
                 'max_length=128,batch_size=4096,'  # real batch_size = 4096/128
                 'learning_rate_constant=2.0').format(2000000)
     hparams_set = f'transformer_tall_{encoder_layers}_{decoder_layers}'
     model = 'transformer'
-    problem = 'translate_class11_pure_envi_iwslt32k'
+    problem = f'translate_class11_pure_{task}_iwslt32k'
     # sleep(5)
     l.append(f"python3 t2t_trainer.py --cloud_tpu_name=grpc://{TPU_ADDRESS}:8470 --model={model} --hparams_set={hparams_set} --hparams={hparams_str} --train_steps={total_train_steps} --eval_steps=20 --problem={problem} --data_dir={train_data_dir} --output_dir={train_output_dir} --use_tpu={use_tpu}")
     # subprocess.Popen(shlex.split(f"python3 t2t_trainer.py --cloud_tpu_name=grpc://{TPU_ADDRESS}:8470 --model=transformer --hparams_set={hparams_set} --hparams={hparams_str} --train_steps={total_train_steps} --eval_steps=20 --problem={problem} --data_dir={train_data_dir} --output_dir={train_output_dir} --use_tpu={use_tpu} > nohup_{task}_{subset}.txt"))
